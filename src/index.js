056dbf17d1f20d9e2ced80cdd64ee640ca69b410
Davina Leong <davina.leong.sy@gmail.com>
2018-10-27 16:33:33 +0800

    Created directory to hold "data" files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App.jsx';
 
-import invoices from './sample-invoices';
-import defaultInvoice from './default-invoice';
+import invoices from './data/sample-invoices';
+import defaultInvoice from './data/default-invoice';
 
 ReactDom.render(<App invoices={invoices} defaultInvoice={defaultInvoice} />, document.getElementById('app'));
