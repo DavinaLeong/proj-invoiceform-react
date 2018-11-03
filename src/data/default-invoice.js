678713086639f439283cfe3cfd251c5c489af6f8
Davina Leong <davina.leong.sy@gmail.com>
2018-11-03 15:08:55 +0800

    Added fields that contain empty data

--- src/data/default-invoice.js
+++ src/data/default-invoice.js
@@ -1,4 +1,11 @@
 export default {
+    id: null,
+    invoiceNo: "",
+    company: "",
+    address: "",
+    date: "",
+    soldTo: "",
+    shipTo: "",
     subtotal: "0.00",
     processingFees: "0.00",
     taxes: "0.00",
