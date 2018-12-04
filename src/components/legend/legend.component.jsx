a94f2ce138468dc55b82f91da5047257ec474477
Davina Leong <davina.leong.sy@gmail.com>
2018-12-04 17:39:23 +0800

    More minor style changes

--- src/components/legend/legend.component.jsx
+++ src/components/legend/legend.component.jsx
@@ -16,17 +16,17 @@ class LegendComponent extends Component {
     render() {
         return (
             <p className="border-bottom pb-1">
-                <span className="badge badge-light text-secondary mr-1">
+                <span className="badge badge-light text-dark mr-1">
                     <i className="fas fa-laptop fa-fw"></i> Work Day</span>
-                <span className="badge badge-light text-secondary mr-1">
+                <span className="badge badge-light text-dark mr-1">
                     <i className="fas fa-calendar fa-fw"></i> Public Holiday</span>
-                <span className="badge badge-light text-secondary mr-1">
+                <span className="badge badge-light text-dark mr-1">
                     <i className="fas fa-building fa-fw"></i> Company Events</span>
-                <span className="badge badge-light text-secondary mr-1">
+                <span className="badge badge-light text-dark mr-1">
                     <i className="fas fa-sun fa-fw"></i> Annual Leave</span>
-                <span className="badge badge-light text-secondary mr-1">
+                <span className="badge badge-light text-dark mr-1">
                     <i className="fas fa-stethoscope fa-fw"></i> Medical Leave</span>
-                <span className="badge badge-light text-secondary">
+                <span className="badge badge-light text-dark">
                     <i className="fas fa-question fa-fw"></i> Undefined</span>
             </p>
         );
