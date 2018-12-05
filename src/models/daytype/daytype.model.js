dc54cd9b233470fef8176bd24514109c382d9555
Davina Leong <davina.leong.sy@gmail.com>
2018-12-05 22:19:45 +0800

    Added schema definition and comments

--- src/models/daytype/daytype.model.js
+++ src/models/daytype/daytype.model.js
@@ -2,19 +2,56 @@ import uuid from 'uuid/v4';
 
 import dayTypeData from './daytype.data';
 
+/**
+ * The model that interfaces with the DayType table data.
+ */
 class DayTypeModel {
 
     constructor() {
         this.data = dayTypeData;
-        this.columns = [
-            'uuid', 'label', 'icon', 'value'
-        ];
     }
 
+    /**
+     * Returns the column information of the DayType table.
+     */
+    schema() {
+        return {
+            tableName: 'daytype',
+            columns: {
+                uuid: {
+                    name: 'uuid',
+                    type: 'string'
+                },
+                label: {
+                    name: 'label',
+                    type: 'string'
+                },
+                icon: {
+                    name: 'icon',
+                    type: 'string'
+                },
+                value: {
+                    name: 'value',
+                    type: 'string'
+                },
+                
+                array: ['uuid', 'label',
+                    'icon', 'value']
+            }
+        };
+    }
+
+    /**
+     * Retrieve all DayTypes
+     */
     findAll() {
         return this.data;
     }
 
+    /**
+     * Retrieve a DayType by its uuid.
+     * @param {string} uuid 
+     */
     findByUuid(uuid) {
         if (! uuid || typeof uuid !== 'string') {
             // TODO: Proper error-handling
@@ -26,8 +63,13 @@ class DayTypeModel {
         });
     }
 
+    /**
+     * Retrieve a DayType by its other fields.
+     * @param {string|number} value 
+     * @param {string|number} key 
+     */
     findOne(value, key) {
-        if (! this.columns.includes(key)) {
+        if (! this.schema.columns.array.includes(key)) {
             // TODO: Proper error-handling
             console.error(`Column '${key} doesn't exist in DayTypes table.`);
             return null;
--- src/models/weeks/week.model.js
+++ src/models/weeks/week.model.js
@@ -1,6 +1,7 @@
 import uuid from 'uuid/v4';
 
 import weekData from './week.data';
+import DayTypeModel from './../daytype/daytype.model';
 
 class WeekModel {
     
@@ -11,19 +12,79 @@ class WeekModel {
         ];
     }
 
-    week(title, company, year, days) {
+    /**
+     * Returns the column information of the Week table.
+     */
+    schema() {
         return {
-            title: title,
-            company: company,
-            year: year,
-            days: days
+            tableName: 'daytype',
+            columns: {
+                uuid: {
+                    name: 'uuid',
+                    type: 'string'
+                },
+                label: {
+                    name: 'label',
+                    type: 'string'
+                },
+                icon: {
+                    name: 'icon',
+                    type: 'string'
+                },
+                value: {
+                    name: 'value',
+                    type: 'string'
+                },
+
+                array: ['uuid', 'title',
+                    'company', 'year', 'days']
+            }
         };
     }
 
+    // week(title, company, year, days) {
+    //     return {
+    //         title: title,
+    //         company: company,
+    //         year: year,
+    //         days: days
+    //     };
+    // }
+
+    // day(date, type, tasks) {
+    //     return {
+    //         date: date,
+    //         type: type,
+    //         tasks: tasks
+    //     }
+    // }
+
+    /**
+     * Retrieve all Weeks.
+     */
     findAll() {
         return this.data;
     }
 
+    /**
+     * Retrieve all Weeks with DayTypes joined.
+     */
+    findAllWithDaytype() {
+        const daytypeSchema = DayTypeModel.schema();
+
+        return this.data.map((week, weekIndex) => {
+            this.data[weekIndex].days.map((day, dayIndex) => {
+                this.data[weekIndex].days[dayIndex].dayType =
+                    DayTypeModel.findOne(day.type,
+                        daytypeSchema.columns.type.name);
+            });
+        });
+    }
+
+    /**
+     * Retrieve a Week by its uuid.
+     * @param {string} uuid 
+     */
     findByUuid(uuid) {
         if (typeof uuid !== 'string') {
             // TODO: Proper error-handling
@@ -35,8 +96,13 @@ class WeekModel {
         });
     }
 
+    /**
+     * Retrieve a Week by its other fields.
+     * @param {string|number} value 
+     * @param {string|number} key 
+     */
     findOne(value, key) {
-        if (! this.columns.includes(key)) {
+        if (! this.schema.columns.array.includes(key)) {
             // TODO: Proper error-handling
             console.error(`Column '${key} doesn't exist in DayTypes table.`);
             return null;
@@ -47,6 +113,10 @@ class WeekModel {
         });
     }
 
+    /**
+     * Create a new Week row.
+     * @param {*} week 
+     */
     create(week) {
         if (! week || typeof week === 'undefined') {
             // TODO: Proper error-handling
@@ -63,9 +133,14 @@ class WeekModel {
 
         this.data.push(createdWeek);
 
-        return createdWeek;
+        return this.data;
     }
 
+    /**
+     * Update an existing Week row by its uuid.
+     * @param {string} uuid 
+     * @param {*} week 
+     */
     update(uuid, week) {
         if (! uuid || typeof uuid === 'undefined' ||
             ! week || typeof week === 'undefined') {
@@ -75,8 +150,8 @@ class WeekModel {
 
         let updatedWeek = null;
         
-        this.data.find((element, index) => {
-            if (element.uuid === uuid) {
+        this.data.find((week, index) => {
+            if (week.uuid === uuid) {
                 this.data[index].title = week.title;
                 this.data[index].company = week.company;
                 this.data[index].year = week.year;
@@ -86,20 +161,92 @@ class WeekModel {
             }
         });
 
-        return updatedWeek;
+        return this.data;
     }
 
+    /**
+     * Update the day of an existing Week row by the day's uuid.
+     * @param {string} uuid 
+     * @param {string} dayUuid 
+     * @param {*} day 
+     */
+    updateDay(uuid, dayUuid, day) {
+        if (! uuid || typeof uuid === 'undefined' ||
+            ! dayUuid || typeof dayUuid === 'undefined' ||
+            ! week || typeof week === 'undefined') {
+            // TODO: Proper error-handling
+            return null;
+        }
+
+        let weekIndex = this.data.find((week, index) => {
+            if (week.uuid === uuid) {
+                return index;
+            }
+        });
+
+        if (this.data[weekIndex].days.length <= 0) {
+            return null;
+        }
+
+        this.data[weekIndex].days.find((day, index) => {
+            if (day.uuid === dayUuid) {
+                this.data[weekIndex].days[index].date = day.date;
+                this.data[weekIndex].days[index].type = day.type;
+                this.data[weekIndex].days[index].tasks = day.tasks;
+            }
+        });
+
+        return this.data;
+    }
+
+    /**
+     * Delete a Week by its uuid.
+     * @param {string} uuid 
+     */
     delete(uuid) {
         if (! uuid || typeof uuid === 'undefined') {
             // TODO: Proper error-handling
             return null;
         }
 
-        this.data.find((element, index) => {
-            if (element.uuid === uuid) {
+        this.data.find((week, index) => {
+            if (week.uuid === uuid) {
                 this.data[index] = null;
             }
         });
+
+        return this.data;
+    }
+
+    /**
+     * Delete a Day of a Week by its uuid.
+     * @param {string} uuid 
+     * @param {string} dayUuid 
+     */
+    deleteDay(uuid, dayUuid) {
+        if (! uuid || typeof uuid === 'undefined' ||
+            ! dayUuid || typeof dayUuid === 'undefined') {
+            // TODO: Proper error-handling
+            return null;
+        }
+
+        let weekIndex = this.data.find((week, index) => {
+            if (week.uuid === uuid) {
+                return index;
+            }
+        });
+
+        if (this.data[weekIndex].days.length <= 0) {
+            return null;
+        }
+
+        this.data[weekIndex].days.find((day, index) => {
+            if (day.uuid === dayUuid) {
+                this.data[weekIndex].days[index] = null;
+            }
+        });
+
+        return this.data;
     }
 
 } // end WeekModel class
